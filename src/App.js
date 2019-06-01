ddf6ddd702b795d32ac010731190e0254382e381
Jeffrey Lancaster <jeffrey.lancaster@gmail.com>
2019-05-31 21:51:32 -0400

    fat arrow, functional constructor

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  let time = new Date()
-  let hours = time.getHours()
-  let minutes = time.getMinutes()
-  let seconds = time.getSeconds()
+const App = () => {
+  const component = new React.Component();
+  
+  component.render = () => {
+    let time = new Date()
+    let hours = time.getHours()
+    let minutes = time.getMinutes()
+    let seconds = time.getSeconds()
 
-  return (
-    <div className="App">
-      <div className="clock">
-        <p>Hours: {hours}</p>
-        <p>Minutes: {minutes}</p>
-        <p>Seconds: {seconds}</p>
+    return (
+      <div className="App">
+        <div className="clock">
+          <p>Hours: {hours}</p>
+          <p>Minutes: {minutes}</p>
+          <p>Seconds: {seconds}</p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  
+  return component
 }
 
 export default App;
