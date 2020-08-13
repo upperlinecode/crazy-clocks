d231602d8a95f3beb54a9b7c585b6522c1890655
jolson615 <jolson615@gmail.com>
2020-08-13 16:07:34 -0400

    functional

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import React from 'react';
 import './App.css';
 
-const App = () => {
-  const component = new React.Component();
-  
-  component.render = () => {
-    let time = new Date()
-    let hours = time.getHours()
-    let minutes = time.getMinutes()
-    let seconds = time.getSeconds()
+function App() {
+  let time = new Date()
+  let hours = time.getHours()
+  let minutes = time.getMinutes()
+  let seconds = time.getSeconds()
 
-    return (
-      <div className="App">
-        <div className="clock">
-          <p>Hours: {hours}</p>
-          <p>Minutes: {minutes}</p>
-          <p>Seconds: {seconds}</p>
-        </div>
+  return (
+    <div className="App">
+      <div className="clock">
+        <p>Hours: {hours}</p>
+        <p>Minutes: {minutes}</p>
+        <p>Seconds: {seconds}</p>
       </div>
-    );
-  }
-  
-  return component
+    </div>
+  );
 }
 
 export default App;
